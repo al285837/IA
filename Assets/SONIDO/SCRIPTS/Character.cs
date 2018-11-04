0162f9aeab5c34b8dcdf9c105d351c0c95bd7e4f
Fran Andújar <al315201@uji.es>
2018-11-04 12:09:47 +0100

    Animator acabado
    Ya estan las animaciones en el animator,

--- Assets/SONIDO/SCRIPTS/Character.cs
+++ Assets/SONIDO/SCRIPTS/Character.cs
@@ -12,6 +12,10 @@ public class Character : MonoBehaviour
     bool crouchW = false;
     bool idle = true;
 
+    bool moving = false;
+    bool crouchPressed = false;
+    bool crouching = false;
+
     // Use this for initialization
     void Start()
     {
@@ -23,12 +27,99 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        crouchPressed = Input.GetKeyDown("v");
+        if (crouchPressed) toggleCrouch();
+        moving = Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f;
 
+        if (idle)
+        {
+            if (moving)
+            {
+                idle = false;
+                sprinting = true;
+            }
+            else if (crouching)
+            {
+                idle = false;
+                crouchI = true;           
+            }
+
+        }
+        else if (crouchI)
+        {
+
+            if(!crouching)
+            {
+                crouchI = false;
+                idle = true;
+            }
+            else if(moving)
+            {
+                crouchI = false;
+                crouchW = true;
+            }
+        }
+        else if (crouchW)
+        {
+            if (!crouching)
+            {
+                crouchW = false;
+                sprinting = true;
+            }
+            else if (!moving)
+            {
+                crouchW = false;
+                crouchI = true;
+            }
+        }
+        else if(sprinting)
+        {
+            if(crouching)
+            {
+                sprinting = false;
+                crouchW = true;
+            }
+            else if(!moving)
+            {
+                sprinting = false;
+                idle = true;
+                    
+            }
+        }
+        actualizarBools();
+        if(sprinting) speed = 5f;
+        if (crouchW) speed = 2f;
+
+
+
+
+    }
+
+
+    void toggleCrouch()
+    {
+        if (crouching) crouching = false;
+        else crouching = true;
+
+    }
+
+    void actualizarBools()
+    {
+        anim.SetBool("CrouchingI", crouchI);
+        anim.SetBool("CWalking", crouchW);
+        anim.SetBool("Sprinting", sprinting);
+        anim.SetBool("Idle", idle);
+    }
+
+
+    void CodigoAparte()
+    {
         if (/*(Input.GetKeyDown("v") && (*/Input.GetAxis("Horizontal") == 0f && Input.GetAxis("Vertical") == 0f/*)) || ((Input.GetAxis("Horizontal") == 0f || Input.GetAxis("Vertical") == 0f) &&crouchW ==true)*/)
         {
-            
 
-            if (Input.GetKeyDown("v")){
+
+            if (Input.GetKeyDown("v"))
+            {
                 if (crouchI == false)
                 {
                     idle = false;
@@ -43,45 +134,39 @@ public class Character : MonoBehaviour
                     idle = true;
                     anim.SetBool("Idle", idle);
                 }
-                
+
             }
-           
 
 
             if (crouchW == true && crouchI == false)
             {
-               
-               /*if (Input.GetKeyDown("v"))
-                {
-                    crouchI = false;
-                    anim.SetBool("CrouchingI", crouchI);
-                    idle = true;
-                    anim.SetBool("Idle", idle);
-                }*/
+
+                /*if (Input.GetKeyDown("v"))
+                 {
+                     crouchI = false;
+                     anim.SetBool("CrouchingI", crouchI);
+                     idle = true;
+                     anim.SetBool("Idle", idle);
+                 }*/
                 crouchW = false;
                 anim.SetBool("CWalking", crouchW);
                 crouchI = true;
                 anim.SetBool("CrouchingI", crouchI);
-                
+
             }
-
-
-           
-
-           
 
 
         }
 
         if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
         {
-           //print("" + idle + ", " + walking + ", " + sprinting);
+            //print("" + idle + ", " + walking + ", " + sprinting);
 
             //print(crouchI);
 
             if (crouchI || sprinting)
             {
-           
+
 
                 if ((Input.GetAxis("Horizontal") <= 0.5f || Input.GetAxis("Vertical") <= 0.5f) && crouchI)
                 {
@@ -91,7 +176,7 @@ public class Character : MonoBehaviour
                     crouchW = true;
                     anim.SetBool("CWalking", crouchW);
 
-               }
+                }
 
                 if ((Input.GetAxis("Horizontal") <= 0.5f || Input.GetAxis("Vertical") <= 0.5f) && sprinting)
                 {
@@ -110,17 +195,16 @@ public class Character : MonoBehaviour
 
                 }*/
             }
-            
 
-            else if((idle || crouchW ) && crouchI ==false)
-            { 
-               
-                    speed = 5f;
-                    sprinting = true;
-                    anim.SetBool("Sprinting", sprinting);
-                      
+
+            else if ((idle || crouchW) && crouchI == false)
+            {
+
+                speed = 5f;
+                sprinting = true;
+                anim.SetBool("Sprinting", sprinting);
+
             }
         }
-
     }
 }
